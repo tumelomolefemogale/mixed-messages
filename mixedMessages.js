ff0238b4897ff230a9ee7b1e7e8681d7f3bc941c
tumelomolefemogale <tmogale554@gmail.com>
2025-11-11 14:52:11 +0200

    Update mixedMessages.js

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,21 +1,8 @@
-const days = ['Monday', 'Friday', 'Sunday'];
-const placesAndactivities = [
-    ' straight to work and finish that one project',
-    ' to the mall and spoil myself',
-    ' to church and praise God'
-];
-const moods = ['happy', 'tired', 'motivated'];
+const theButton = document.getElementById('the-button')
+const funnyImage = document.getElementById('funny-image');
 
-const sentenceButton = document.getElementById('sentence-button');
-const sentence = document.getElementById('sentence')
-
-function randomSentence() {
-    const randomIndex1 = Math.floor(Math.random() * days.length);
-    const randomIndex2 = Math.floor(Math.random() * placesAndactivities.length);
-    const randomIndex3 = Math.floor(Math.random() * moods.length);
-
-    const randomSentence = `Today is ${days[randomIndex1]}, I feel ${moods[randomIndex3]} but I'd rather go ${placesAndactivities[randomIndex2]}.`;
-    sentence.innerHTML = randomSentence;
+function displayImage() {
+    funnyImage.style.display = 'block'
 }
 
-sentenceButton.addEventListener('click', randomSentence)
+theButton.addEventListener('click', displayImage)
