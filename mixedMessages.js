0b5a8996b1ad4ee53c1dd46d18834bd2dfc103a1
tumelomolefemogale <tmogale554@gmail.com>
2025-11-11 17:53:56 +0200

    Update mixedMessages.js

--- mixedMessages.js
+++ mixedMessages.js
@@ -2,6 +2,9 @@ const theButton = document.getElementById('the-button')
 const funnyImage = document.getElementById('funny-image');
 const sentence = document.getElementById('sentence');
 
+const surveyLabels = document.querySelectorAll('.survey-label');
+const surveyInputs = document.querySelectorAll('.survey-input');
+
 function displayImage() {
     funnyImage.style.display = 'block';
     setTimeout(() => {
@@ -10,6 +13,9 @@ function displayImage() {
 
     sentence.innerHTML = 'WARNING: Smiling gorilla may cause spontaneous envy.'
     theButton.style.display = 'none'
+
+    surveyLabels.forEach(label => label.style.display = 'none');
+    surveyInputs.forEach(input => input.style.display = 'none');
 }
 
 theButton.addEventListener('click', displayImage)
