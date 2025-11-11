7bb1f07c339a04bcb7dec948e1fb0764be8cdd05
tumelomolefemogale <tmogale554@gmail.com>
2025-11-11 15:10:08 +0200

    Update mixedMessages.js

--- mixedMessages.js
+++ mixedMessages.js
@@ -2,7 +2,8 @@ const theButton = document.getElementById('the-button')
 const funnyImage = document.getElementById('funny-image');
 
 function displayImage() {
-    funnyImage.style.display = 'block'
+    funnyImage.style.display = 'block';
+    theButton.style.display = 'none'
 }
 
 theButton.addEventListener('click', displayImage)
